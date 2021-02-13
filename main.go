3c32934e568527a3d26020548f3db0650767bc00
Steve Kim <emailstevenkim@gmail.com>
2021-02-12 23:31:21 -0500

    Chapter 6 - Add bootstrap layout and navbar

--- main.go
+++ main.go
@@ -13,21 +13,23 @@ var contactView *views.View
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	data := struct{ Name string }{Name: "Steve"}
-	if err := homeView.Template.Execute(w, data); err != nil {
+	// Execute a template name homeView.Layout ("bootstrap") writing the results to w and passing data
+	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, data); err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.Execute(w, nil); err != nil {
+	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	// Pass the name of the layout to use and specify the view to parse
+	homeView = views.NewView("bootstrap","views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
--- views/view.go
+++ views/view.go
@@ -3,19 +3,24 @@ package views
 import "html/template"
 
 type View struct {
+	Layout   string
 	Template *template.Template
 }
 
 // files is a variadic parameter that can accept 0...n arguments; files is treated as a slice in the NewView function
-// NewView(someSlice...) to unravel the items in slice when invoking a variadic function
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+// ewView(someSlice...) to unravel the items in slice when invoking a variadic function
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+		"views/layouts/navbar.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 
 	return &View{
+		Layout:   layout,
 		Template: t,
 	}
 }

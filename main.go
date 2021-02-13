24c1b35f9443d08ea6de21d30d440e748c253611
Steve Kim <emailstevenkim@gmail.com>
2021-02-12 22:06:26 -0500

    Chapter 6 - Set up templates

--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// Global variable for storing the / template
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello, World!</h1>")
+	data := struct{ Name string }{Name: "Steve"}
+	// Execute the template indicating where to write the template output and the data to pass in to the template
+	if err := homeTemplate.Execute(w, data); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	var err error
+	// Parse the home.gohtml template
+	homeTemplate, err = template.ParseFiles(
+		"views/home.gohtml",
+		"views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles(
+		"views/contact.gohtml",
+		"views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)

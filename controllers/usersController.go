62da28815adca7d9250612071c9d22da6692ceb0
Steve Kim <emailstevenkim@gmail.com>
2021-02-13 00:23:34 -0500

    Chapter 7 - Create a users controller

---
+++ controllers/usersController.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/schema"
+
+	"lenslocked.com/views"
+)
+
+type Users struct {
+	NewView *views.View
+}
+
+// Struct tags are metadata that can be added to fields of any struct
+// gorilla/schema package unmarshals JSON into a struct
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Email is", form.Email)
+	fmt.Fprintln(w, "Password is", form.Password)
+}
+
+func parseForm(r *http.Request, dst interface{}) error {
+	// ParseForm parses a form on a request and stores the data in PostForm in the request
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	dec := schema.NewDecoder()
+	if err := dec.Decode(dst, r.PostForm); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+
+
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"net/http"
 
-	"lenslocked.com/views"
-
 	"github.com/gorilla/mux"
+
+	"lenslocked.com/controllers"
+	"lenslocked.com/views"
 )
 
 var homeView *views.View
 var contactView *views.View
-var signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -24,11 +24,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 // A helper function that panics on any error
 func must(err error) {
 	if err != nil {
@@ -40,13 +35,13 @@ func main() {
 	// Pass the name of the layout to use and specify the view to parse
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
-
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
 	http.ListenAndServe(":3000", r)
 }
